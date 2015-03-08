a2413be25c2b25776751a3c19d2292785655a42d
Tomasz Ducin <tomasz.ducin@gmail.com>
2015-03-09 00:09:41 +0100

    aglio rendering along with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    aglio: {
+      docs: {
+        files: {
+          'index.html': 'api.md',
+        },
+        options: {
+          theme: "slate"
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          hostname: 'localhost',
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-aglio');
+
+  grunt.registerTask('default', ['aglio', 'connect']);
+};
